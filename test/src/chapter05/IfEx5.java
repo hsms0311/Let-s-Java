fd91fe8ea174c157bce659b7546255925b57fc62
JW <yjw9290@gmail.com>
2024-10-31 23:39:23 +0900

    2day

---
+++ test/src/chapter05/IfEx5.java
@@ -0,0 +1,18 @@
+package chapter05;
+
+public class IfEx5
+{
+
+	public static void main(String[] args)
+	{
+		int math = 80;
+		int eng = 60;
+		
+		if (math >= 60 && eng >= 60) {
+			System.out.println("합격");
+		} else {
+			System.out.println("불합격");
+		}
+	}
+
+}
