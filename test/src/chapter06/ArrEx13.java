2b642866b2318490acbfe416137ffd91d3c378b1
JW <yjw9290@gmail.com>
2024-11-03 22:12:41 +0900

    5day

---
+++ test/src/chapter06/ArrEx13.java
@@ -0,0 +1,20 @@
+package chapter06;
+
+public class ArrEx13
+{
+
+	public static void main(String[] args)
+	{
+		String[] names = {"홍길동", "이순신", "김유신"};
+		
+		int[] score = {90, 80, 100};
+		
+		int i = 0; // 인덱스 변수.
+		
+		for (String name : names) {
+			System.out.println(name + " : " + score[i]);
+			i++;
+		}
+	}
+
+}
